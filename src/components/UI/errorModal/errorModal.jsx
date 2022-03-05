8b62abd712859de24e01e247deee4936634f1119
Gent Jashari <gentjashari1997@gmail.com>
2022-03-06 00:20:46 +0100

    adding portals to erroModal

--- src/components/UI/errorModal/errorModal.jsx
+++ src/components/UI/errorModal/errorModal.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import classes from './errorModal.module.scss'
 import Card from '../Card';
 import Button from '../Button/button';
 
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.errorHandler}></div>
+}
+
+const ModalOverlay = props => {
+    return <Card className={classes.modal}>
+        <header className={classes.header}>
+            <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.errorHandler}>Close Modal</Button>
+        </footer>
+    </Card>
+}
+
 const ErrorModal = props => {
     return (
         <>
-            <div className={classes.backdrop} onClick={props.errorHandler}></div>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.errorHandler}>Close Modal</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(
+                <Backdrop errorHandler={props.errorHandler} />,
+                document.getElementById("backdrop-root"))
+            }
+
+            {
+                ReactDOM.createPortal(
+                    <ModalOverlay title={props.title} message={props.message} onClick={props.onClick} />,
+                    document.getElementById('modal-root')
+                )
+            }
+
+
         </>
     )
 }
